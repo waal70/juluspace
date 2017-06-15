fb1723c71547f0221ae547e473aedeb3405ceffa
André <waal70@hotmail.com>
2017-06-15 17:00:38 +0200

    Added ArduinoCode + libraries

---
+++ ArduinoCode/serial_communication/serial_communication.ino
@@ -0,0 +1,25 @@
+/*
+ * Hello World!
+ *
+ * This is the Hello World! for Arduino. 
+ * It shows how to send data to the computer
+ */
+long i;
+
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(115200);           // set up Serial library at 115200 bps
+  
+  Serial.println("BEGIN");  // prints hello with ending line break 
+  i = 0;
+}
+
+void loop()                       // run over and over again
+{
+
+  Serial.print("Dit is een hele lange string zonder newline karakter");
+  Serial.println(i);
+  i++;
+  delay(5000);
+                                  // do nothing!
+}
